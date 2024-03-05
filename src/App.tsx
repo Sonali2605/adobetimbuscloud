06617144ac45385ededd1001472c954a178e9563
sohel.pathan <sohel.pathan@harbingergroup.com>
2024-03-05 17:17:07 +0530

    Profile Page Added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Button } from "@/components/ui/button"
 import { Switch } from "@/components/ui/switch"
 import Login from "./project-components/Login"
 import Dashboard from "./project-components/Dashboard"
+import ProfilePage from "./project-components/profilePage"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 export default function App(){
   return (
     <>
    {/* <Login/> */}
    {/* <Dashboard/> */}
+   {/* <ProfilePage /> */}
    <BrowserRouter>
    <Routes>
     <Route path ="/" element={<Login/>}/>
