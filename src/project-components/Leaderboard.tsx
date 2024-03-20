e571126464fd12bade3d7f8a750e8ce4ac366adc
sonal.pawar <sonal.pawar@harbingergroup.com>
2024-03-20 18:26:37 +0530

    updated leaderboard and partnership dashboard

--- src/project-components/Leaderboard.tsx
+++ src/project-components/Leaderboard.tsx
@@ -1,6 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import {useNavigate } from "react-router-dom";
+import axios from 'axios';
+
+interface Course {
+  id: string; // Assuming 'id' is a required property in your data
+  attributes: {
+    imageUrl: string;
+    localizedMetadata?: {
+      name: string;
+    }[];
+  };
+  state?: string; // Define 'state' as an optional property
+}
 
 const Leaderboard = () => {
+  const [courseData, setCourseData] = useState<Course[]>([]);
+  const navigate = useNavigate();
+
   // Dummy data for network members with existing user images
   const networkMembers = [
     { name: "88 points", imageUrl: "https://randomuser.me/api/portraits/men/1.jpg" },
@@ -28,6 +44,53 @@ const Leaderboard = () => {
 
   const [expanded, setExpanded] = useState(false);
 
+  useEffect(() => {
+    getCoursestoExplore();
+  }, []);
+
+  async function getCoursestoExplore() {
+    try {
+      const token = localStorage.getItem("access_token");
+      const config = {
+        headers: { Authorization: `oauth ${token}` },
+      };
+      const response = await axios.get(
+        `https://learningmanager.adobe.com/primeapi/v2/learningObjects?page[limit]=20&filter.catalogIds=174772&sort=name&filter.ignoreEnhancedLP=true`,
+        config
+      );
+      const result = response?.data?.data;
+      setCourseData(result);
+    } catch (error) {
+      console.error("Error fetching learning objects:", error);
+    }
+  }
+
+  const  EnrollHandle = async(cid:string) =>{
+    const course = (courseData as any).find(obj => obj?.id === cid);
+     const Iid =  course.relationships?.instances?.data?.[0].id;
+     
+     console.log("nnnnnnn", course, Iid)
+     const token = localStorage.getItem("access_token")
+     try {
+         const response = await fetch('https://learningmanager.adobe.com/primeapi/v2/enrollments?loId=' + cid + '&loInstanceId=' + encodeURIComponent(Iid), {
+             method: 'POST',
+             headers: {
+                 'Content-Type': 'application/json',
+                 'Authorization': `Bearer ${token}`
+             },
+         });
+ 
+         if (!response.ok) {
+             navigate('/')
+             throw new Error('Failed to enroll');
+         } else {
+           navigate(`/learning_object/${cid}/instance/${Iid}/isLearning=false/isCustomer=false/detailspage`);
+         }
+     } catch (error) {
+         console.log(error)
+     }
+    
+   }
   const toggleExpanded = () => {
     setExpanded(!expanded);
   };
@@ -66,16 +129,16 @@ const Leaderboard = () => {
         <div className="mt-4">
           <h2 className="text-md font-bold mt-10 mb-8">Courses Taken By Your Peers</h2>
           <div className="grid grid-cols-4 gap-6">
-            {courses.map((course, index) => (
+          {courseData.map((course, index) => (
               <div key={index} className="max-w-xs bg-white overflow-hidden shadow-lg rounded-lg px-4">
                 <img
                   className="w-full h-40 object-cover mb-4 rounded-t-lg"
-                  src={course.imageUrl}
-                  alt={course.name}
+                  src={course?.attributes?.imageUrl}
+                  alt={course?.attributes?.localizedMetadata?.[0]?.name || ''}
                 />
                 <div className="px-3">
-                  <div className="text-sm mb-2 text-center">{course.name}</div>
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded w-full text-sm mb-3">
+                  <div className="text-sm mb-2 text-center">{course?.attributes?.localizedMetadata?.[0]?.name}</div>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded w-full text-sm mb-3" onClick={()=>EnrollHandle(course?.id)}>
                     Explore
                   </button>
                 </div>
